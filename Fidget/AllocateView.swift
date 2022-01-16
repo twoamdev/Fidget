5d8d18460de088c2588667bcfdc245037becfe21
Benjamin Nelson <twoamdev@gmail.com>
2022-01-16 05:55:29 -0500

    adding some view features

--- Fidget/AllocateView.swift
+++ Fidget/AllocateView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct AllocateView: View {
+    @State private var userMessage: String = ""
+    @State private var moneyAmount: String = ""
     var body: some View {
-        Color.red
-            .overlay(
-                Text("ALLOCATE VIEW")
-            )
+        
+        VStack{
+            
+            Text("Allocate")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(Color.white)
+                
+           
+            Spacer()
+            HStack{
+                TextField("/ :", text: $userMessage)
+                
+                    .padding(EdgeInsets(top: 15, leading: 10, bottom: 15, trailing: 0))
+                    .textFieldStyle(.roundedBorder)
+                    
+                
+                TextField("$", text: $moneyAmount)
+                
+                    .padding(EdgeInsets(top: 15, leading: 0, bottom: 15, trailing: 0))
+                    .textFieldStyle(.roundedBorder)
+                    .keyboardType(.decimalPad)
+                
+                
+                Button("Send"){
+                    
+                }
+                .foregroundColor(Color.white)
+                    
+                        
+                Spacer()
+            }
+            .background(Color.blue)
+            
+            
+        }
+        
         
     }
     
