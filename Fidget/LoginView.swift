43ee40bd81db7a10859e1df871b859f91f87ef2b
Benjamin Nelson <twoamdev@gmail.com>
2022-01-17 20:09:17 -0500

    changed color palette and added custom Fonts

--- Fidget/LoginView.swift
+++ Fidget/LoginView.swift
@@ -8,49 +8,66 @@
 import SwiftUI
 
 
-
 struct LoginView: View {
     @EnvironmentObject var appState: AppState
     
     @State private var username: String = ""
     @State private var password: String = ""
+   
+    
+    let appFontPrimary = "DMSans-Regular"
     var body: some View {
         
         VStack{
             Spacer()
             Text("Fidget")
-                .font(.title)
+            //.font(.title)
+                .font(Font.custom(appFontPrimary,size: 40, relativeTo: .title))
+                .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
+                .foregroundColor(ColorPallete().tempTitleColor)
             TextField("Username", text: $username)
+                .font(Font.custom(appFontPrimary,size:15))
                 .padding(EdgeInsets(top: 0, leading: 50, bottom: 0, trailing: 50))
             
                 .textFieldStyle(.roundedBorder)
+                .foregroundColor(ColorPallete().tempFGColor)
             SecureField("Password", text: $password)
+                .font(Font.custom(appFontPrimary,size:15))
                 .padding(EdgeInsets(top: 0, leading: 50, bottom: 10, trailing: 50))
                 .textFieldStyle(.roundedBorder)
-            Button("login"){
-                appState.loggedIn = true
+                .foregroundColor(ColorPallete().tempFGColor)
+            HStack(){
+                Spacer()
+                Button("Log In"){
+                    appState.loggedIn = true
+                }
+                .font(Font.custom(appFontPrimary,size:15))
+                .foregroundColor(.white)
+                .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
+                .background(ColorPallete().tempFGColor)
+                .cornerRadius(7)
             }
-            .foregroundColor(.white)
-            .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
-            .background(Color.blue)
-            .cornerRadius(25)
+            .padding(EdgeInsets(top: 0, leading: 50, bottom: 10, trailing: 50))
+            
             
             
             
             Spacer()
             Spacer()
         }
-        
+            
+            .background(ColorPallete().tempBGColor)
         
         
         
         
     }
+        
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-       LoginView()
+        LoginView()
     }
 }
 
---
+++ Fidget/StyleUtils.swift
@@ -0,0 +1,17 @@
+//
+//  StyleUtils.swift
+//  Fidget
+//
+//  Created by Benjamin Nelson on 1/17/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorPallete{
+    let tempBGColor = Color(red: 228/255, green: 221/255, blue: 222/255)
+    let tempFGColor = Color(red: 138/255, green: 108/255, blue: 112/255)
+    let tempTitleColor = Color(red: 255/255, green: 255/255, blue: 255/255)
+    
+}
+//.background(Color(red: 228/255, green: 221/255, blue: 222/255))
